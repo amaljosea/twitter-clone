ab2a8076a4c13768d0bd18dc9b0a026b30b60abe
amal jose <amal.jose@appsintegra.com>
2020-03-24 17:50:34 +0530

    Add login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import Header from '../components/Header'
 import ImageBackground from '../components/ImageBackground'
+import Container from '../components/Container'
+import { Button, Form } from 'react-bootstrap'
 
 const Login = () => {
     return (
         <ImageBackground>
-            <Header />
+            <Container>
+                <Header />
+                <Button variant="primary">Create Account</Button>
                 Login
+                <div style={{ width: "30vw" }}>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control type="email" placeholder="Email" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Login
+                    </Button>
+                    </Form>
+                </div>
+            </Container>
         </ImageBackground>)
 }
 
