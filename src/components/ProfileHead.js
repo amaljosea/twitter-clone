c8a0e5b82e1ac59a0d0eb48128ba9dbf88831202
amal jose <amal.jose@appsintegra.com>
2020-03-25 22:24:37 +0530

    Profile page style fix

--- src/components/ProfileHead.js
+++ src/components/ProfileHead.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { ListGroup } from 'react-bootstrap'
-import Avatar from '../images/cardAvatar.png'
 import './ProfileHead.css'
 
 const ProfileHead = () => {
     return (
         <ListGroup.Item className="user-list-content">
             <div className="user-list-text-container">
-                <img src={Avatar} alt="avatar" variant="start" className="feed-card-avatar" />
+                <div>
+                    <div className="profile-head-avathar"/>
+                </div>
                 <div>
                     <h4>Cras justo odio</h4>
                     <div className="user-list-details">
