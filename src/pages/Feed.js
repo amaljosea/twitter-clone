b88867de04f39035f20f05ea0507bfa7b7696b3a
amal jose <amal.jose@appsintegra.com>
2020-03-25 09:09:03 +0530

    Add Tabs container

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -2,26 +2,20 @@ import React from 'react'
 import Header from '../components/Header'
 import Container from '../components/Container'
 import FeedCard from '../components/FeedCard'
+import TabsContainer from '../components/TabsContainer'
 import "./Feed.css"
 import { Button } from 'react-bootstrap'
-import { Row, Col } from 'react-bootstrap'
-
 
 const Feed = () => {
     return (
         <Container>
             <Header displayLinks />
-            <Row className="card-row">
-                <Col sm={12} md={12} xl={6}>
-                    <div>
-                        <Button >Write</Button>
-                        <FeedCard />
-                        <FeedCard />
-                        <FeedCard />
-
-                    </div>
-                </Col>
-            </Row>
+            <TabsContainer>
+                <Button >Write</Button>
+                <FeedCard />
+                <FeedCard />
+                <FeedCard />
+            </TabsContainer>
         </Container >
     )
 }
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Header from '../components/Header'
 import Container from '../components/Container'
+import { ListGroup } from 'react-bootstrap'
+import TabsContainer from '../components/TabsContainer'
 
 export const Users = () => {
     return (
         <Container>
             <Header displayLinks />
-            <div>
-                Users
-            </div>
+            <TabsContainer>
+                <ListGroup>
+                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                    <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+                    <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+            </TabsContainer>
         </Container>
     )
 }
