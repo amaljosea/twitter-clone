375205abe1b6611faec2b249302233e8f4f3b34c
amal jose <amal.jose@appsintegra.com>
2020-03-26 07:23:22 +0530

    Add feed section

--- src/components/FeedList.js
+++ src/components/FeedList.js
@@ -7,6 +7,9 @@ export const UserList = () => {
             <FeedCard />
             <FeedCard />
             <FeedCard />
+            <FeedCard />
+            <FeedCard />
+            <FeedCard />
         </>
     )
 }
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Container from '../components/Container'
 import FeedList from '../components/FeedList'
 import TabsContainer from '../components/TabsContainer'
 import "./Feed.css"
-import { Button } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
 
 const Feed = () => {
+    const [tweet, setTweet] = useState("")
+    const [showNewTweet, setShowNewTweet] = useState(false)
+
+    const handleSubmit = () => {
+
+    }
     return (
         <Container>
             <Header displayLinks />
             <TabsContainer>
-                <Button >Write</Button>
+                {showNewTweet ? <section>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group >
+                            <Form.Control as="textarea" rows="3" placeholder="What is happening?" value={tweet} required onChange={(e) => {
+                                setTweet(e.target.value)
+                            }} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" className="submit-tweet">
+                            Submit
+                        </Button>
+                        <Button variant="secondary" onClick={()=>setShowNewTweet(false)}>
+                            Discard
+                        </Button>
+                    </Form>
+                </section> : <Button onClick={() => setShowNewTweet(true)}>Write</Button>}
                 <FeedList />
             </TabsContainer>
         </Container >
     )
 }
+
 
 export default Feed
